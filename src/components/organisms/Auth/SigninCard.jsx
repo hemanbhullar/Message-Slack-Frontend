4bb63e05750ec21cf18e8ee7d5681a46e220399b
hemanbhullar <hemanbhullar8082@gmail.com>
2025-01-24 23:26:40 +0530

    Added signin card

---
+++ src/components/organisms/Auth/SigninCard.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+
+export const SigninCard = () => {
+
+  const navigate = useNavigate();
+
+  const [signinForm, setSigninForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  return (
+    <>
+      <Card className="w-full h-full">
+        <CardHeader>
+          <CardTitle>Sign In</CardTitle>
+          <CardDescription>Sign in to access your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-3">
+            <Input
+              placeholder="Email"
+              required
+              onChange={(e) =>
+                setSigninForm({ ...signinForm, email: e.target.value })
+              }
+              value={signinForm.email}
+              type="email"
+              disabled={false}
+            />
+            <Input
+              placeholder="Password"
+              required
+              onChange={(e) =>
+                setSigninForm({ ...signinForm, password: e.target.value })
+              }
+              value={signinForm.password}
+              type="password"
+              disabled={false}
+            />
+            <Button
+                disabled={false}
+                size='lg'
+                type='submit'
+                className='w-full'
+            >
+                Continue
+            </Button>
+          </form>
+          <Separator className='my-5' />
+          <p className='text-center text-sm text-muted-foreground mt-4'>
+                    Donot have an account ? {' '}
+                    <span 
+                      className='text-sky-600 hover:underline cursor-pointer'
+                      onClick={() => navigate('/auth/signup')}
+                    >
+                      Sign Up
+                    </span>
+            </p>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
--- src/components/organisms/Auth/SignupCard.jsx
+++ src/components/organisms/Auth/SignupCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
@@ -6,6 +7,8 @@ import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 
 export const SignupCard = () => {
+
+    const navigate = useNavigate();
 
     const [signupForm, setSignupForm] = useState({
         email: '',
@@ -67,7 +70,12 @@ export const SignupCard = () => {
                 <Separator className='my-5'/>
                 <p className='text-center text-sm text-muted-foreground mt-4'>
                     Already have an account ? {' '}
-                    <span className='text-sky-600 hover:underline cursor-pointer'>Sign In</span>
+                    <span 
+                      className='text-sky-600 hover:underline cursor-pointer'
+                      onClick={() => navigate('/auth/signin')}
+                    >
+                      Sign In
+                    </span>
                 </p>
             </CardContent>
           </Card>
