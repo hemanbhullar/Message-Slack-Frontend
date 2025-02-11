c2648de042a2522917a2d3364b9a5008b32d877f
hemanbhullar <hemanbhullar8082@gmail.com>
2025-02-11 07:29:14 +0530

    Consumed useFetchWorkspaces

--- src/apis/workspaces/index.js
+++ src/apis/workspaces/index.js
@@ -22,15 +22,15 @@ export const createWorkspaceRequest = async ({ name, description, token }) => {
 
 export const fetchWorkspacesRequest = async ({ token }) => {
     try {
-        const response = await axios.get('/workspaces', {
+        console.log('token', token);
+        const response = await axios.get('/workspaces/userworkspaces', {
             headers: {
                 'x-access-token': token
             }
         });
 
         console.log('Response in fetch workspace request', response);
-
-        return response?.data;
+        return response?.data?.data;
     } catch (error) {
         console.log('Error in fetching workspace request');
         throw error.response.data;
--- src/hooks/apis/workspaces/useFetchWorkspace.js
+++ src/hooks/apis/workspaces/useFetchWorkspace.js
@@ -4,11 +4,11 @@ import { fetchWorkspacesRequest } from '@/apis/workspaces';
 import { useAuth } from '@/hooks/context/useAuth';
 
 export const useFetchWorkspace = () => {
-    const auth = useAuth();
+    const {auth} = useAuth();
 
     const { isFetching, isSuccess, error, data: workspaces } = useQuery({
         queryFn: () => fetchWorkspacesRequest({ token: auth?.token }),
-        queryKey: 'fetchWorkspaces',
+        queryKey: ['fetchWorkspaces'],
         staleTime: 30000
     });
 
