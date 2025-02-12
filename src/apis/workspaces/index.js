050e18d2bac633aa9607f182a52a97d0502b9c9c
hemanbhullar <hemanbhullar8082@gmail.com>
2025-02-12 08:29:11 +0530

    Added get Workspace by id api

--- src/apis/workspaces/index.js
+++ src/apis/workspaces/index.js
@@ -22,14 +22,27 @@ export const createWorkspaceRequest = async ({ name, description, token }) => {
 
 export const fetchWorkspacesRequest = async ({ token }) => {
     try {
-        console.log('token', token);
         const response = await axios.get('/workspaces/userworkspaces', {
             headers: {
                 'x-access-token': token
             }
         });
 
-        console.log('Response in fetch workspace request', response);
+        return response?.data?.data;
+    } catch (error) {
+        console.log('Error in fetching workspace request');
+        throw error.response.data;
+    }
+};
+
+export const fetchWorkspacesDetailsRequest = async ({ workspaceId, token }) => {
+    try {
+        const response = await axios.get(`/workspaces/${workspaceId}}`, {
+            headers: {
+                'x-access-token': token
+            }
+        });
+
         return response?.data?.data;
     } catch (error) {
         console.log('Error in fetching workspace request');
