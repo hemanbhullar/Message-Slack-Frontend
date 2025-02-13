814373a08c86d6316a4441531bb72fb773b24242
hemanbhullar <hemanbhullar8082@gmail.com>
2025-02-13 09:02:21 +0530

    Added complete implementation for workspace switcher

--- src/components/organisms/Workspace/WorkspaceSwitcher.jsx
+++ src/components/organisms/Workspace/WorkspaceSwitcher.jsx
@@ -1,27 +1,61 @@
-import { Loader } from 'lucide-react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Loader } from "lucide-react";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { useGetWorkspaceById } from '@/hooks/apis/workspaces/useGetWorkspaceById';
+import { fetchWorkspacesDetailsRequest } from "@/apis/workspaces";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useFetchWorkspace } from "@/hooks/apis/workspaces/useFetchWorkspace";
+import { useGetWorkspaceById } from "@/hooks/apis/workspaces/useGetWorkspaceById";
 
 export const WorkspaceSwitcher = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { workspaceId } = useParams();
 
-    const { workspaceId }  = useParams();
+  const { isFetching, workspace } = useGetWorkspaceById(workspaceId);
+  const { workspaces, isFetching: isFetchingWorkspace } = useFetchWorkspace();
 
-    const { isFetching, workspace} = useGetWorkspaceById(workspaceId);
-
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <Button 
-                    className='size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 font-semibold text-slate-800 text-xl' 
-                >
-                    {isFetching? (<Loader className='size-5 animate-spin' />) : workspace?.name.charAt(0).toUpperCase()}
-                </Button>
-            </DropdownMenuTrigger>
-        </DropdownMenu>
-    );
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 font-semibold text-slate-800 text-xl">
+          {isFetching ? (
+            <Loader className="size-5 animate-spin" />
+          ) : (
+            workspace?.name.charAt(0).toUpperCase()
+          )}
+        </Button>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className='cursor-pointer flex-col justify-start items-start capitalize'
+          >
+            {workspace?.name}
+            <span className="text-xs text-muted-foregorund">
+              (Active Workspace)
+            </span>
+          </DropdownMenuItem>
+          {isFetchingWorkspace ? (
+            <Loader className="size-5 animate-spin" />
+          ) : (
+            workspaces?.map((workspace) => {
+              if (workspace._id === workspaceId) return;
+              return (
+                <DropdownMenuItem key={workspace._id}
+                 onClick={() => navigate(`/workspaces/${workspace._id}`)}
+                 className='cursor-pointer flex-col justify-start items-start capitalize'>
+                  <p className="truncate">{workspace?.name}</p>
+                </DropdownMenuItem>
+              );
+            })
+          )}
+        </DropdownMenuContent>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
